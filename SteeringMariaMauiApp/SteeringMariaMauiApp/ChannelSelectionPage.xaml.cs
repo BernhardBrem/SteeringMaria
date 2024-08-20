d8fb550c6b2debe30ac25e6117c1c3424f95f7ef
Bernhard Brem <mail@bernhard-brem.de>
2024-08-20 20:49:54 +0200

    passtscho

---
+++ SteeringMariaMauiApp/SteeringMariaMauiApp/ChannelSelectionPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace SteeringMariaMaui;
+
+public partial class ChannelSelectionPage : ContentPage
+{
+	int count = 0;
+
+	public ChannelSelectionPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
