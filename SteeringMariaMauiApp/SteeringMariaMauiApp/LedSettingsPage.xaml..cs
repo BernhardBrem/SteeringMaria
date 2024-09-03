69e71fe2862bf4be07c97099f231dbec53a99510
Bernhard Brem <mail@bernhard-brem.de>
2024-09-03 22:16:41 +0200

    passtscho

--- SteeringMariaMauiApp/SteeringMariaMauiApp/LedSettingsPage.xaml..cs
+++ SteeringMariaMauiApp/SteeringMariaMauiApp/LedSettingsPage.xaml..cs
@@ -7,6 +7,7 @@ public partial class LedSettingsPage : ContentPage
 {
 	Dictionary<string,LedControler.LedSettings> LedSettings=new Dictionary<string, LedControler.LedSettings>(); 
 	LedControler ledControler=new LedControler();
+	string ledName="";
 
 	public LedSettingsPage()
 	{
@@ -15,12 +16,38 @@ public partial class LedSettingsPage : ContentPage
 		ledControler.RequestLedSettings();
 	
 	}
+
+	void setSurfaceToLed(){
+		var settings=LedSettings[ledName];
+		Channel.Text=settings.channel.ToString();
+	    Brightnes.Value=settings.brightnes;
+	    BrightnesSpan.Value=settings.brightnesspan;
+		BrightnesFactor.Value=settings.brightnesfactor;
+	}
+
+	void OnLedChanged(object sender, EventArgs e)
+    {
+  		var picker = (Picker)sender;
+  		int selectedIndex = picker.SelectedIndex;
+  		if (selectedIndex != -1)
+  		{
+    		ledName = (string)picker.Items[selectedIndex];
+			setSurfaceToLed();
+  		}
+	}
     
 	void OnChannelTextChanged(object sender,  TextChangedEventArgs e){}
 
 	void OnChannelCompleted (object sender,  EventArgs e) {}
 
 	void OnBrightnesSliderValueChanged(object sender,  ValueChangedEventArgs e) {}
+
+	void OnBrightnesSpanSliderValueChanged(object sender,  ValueChangedEventArgs e) {}
+	
+	void OnBrightnesFactorSliderValueChanged(object sender,  ValueChangedEventArgs e) {}
+    
+	
+
     
 	public void OnSettingsReady(object sender,EventArgs args){
 		Trace.WriteLine("Updating LED Names");
